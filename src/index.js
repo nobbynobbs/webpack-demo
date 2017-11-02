8577892d478036d3fe8115ead9260d0ebf27fbac
rbolkhovitin <brv@studio-tg.ru>
2017-11-02 18:43:03 +0300

    add images loading

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon.png';
 
 function component() {
-    let element = document.createElement('div');
+  let element = document.createElement('div');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
 
-    return element;
+  // Add the image to our existing div.
+  let myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  return element;
 }
 
 document.body.appendChild(component());
