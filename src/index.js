0b2310a20976d9460137a27b84ac7ba06940b066
rbolkhovitin <brv@studio-tg.ru>
2017-11-07 15:38:43 +0300

    add math module
    (cherry picked from commit 406e7c2)

--- src/index.js
+++ src/index.js
@@ -1,30 +1,18 @@
-import _ from 'lodash';
-import printMe from './print.js';
-import './styles.css'
+import {cube} from './math.js';
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+  var element = document.createElement('pre');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
+  element.innerHTML = [
+    'Hello webpack!',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
 
   return element;
 }
 
 document.body.appendChild(component());
-
-
-if (module.hot) {
-  module.hot.accept('./print.js', function () {
-    console.log('Accepting the updated printMe module!');
-    printMe();
-  })
-}
